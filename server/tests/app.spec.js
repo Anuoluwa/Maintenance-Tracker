f1e3c3bd3459a5f53f5d1f3d417d7dd46403ef7d
Anu Apiti <anuapt@gnail.com>
2018-05-15 17:27:28 -0700

    [Feature #157559903] Fix test bug, bug of 'EARINUSE::3000' rewrite tests it fails

--- server/tests/app.spec.js
+++ server/tests/app.spec.js
@@ -1,12 +1,13 @@
-import request from 'supertest';
-import app from '../app';
+import supertest from 'supertest';
+// import expect from 'chai';
+// import app from '../app';
 
-describe('GET /requests', () => {
-  it('respond with json', (done) => {
-    request(app)
-      .get('/requests')
+const url = supertest('http://localhost:3000');
+
+describe(' /GET Requests', () => {
+  it('it should return a 200 OK response', (done) => {
+    url.get('/requests')
       .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
       .expect(200, done);
   });
 });
