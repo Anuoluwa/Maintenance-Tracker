5cdeb555f11d4a4b499c3836ce1a5b2b17e99b4e
Anu Apiti <anuapt@gnail.com>
2018-05-16 17:25:10 -0700

    [Feature #157560455] Write integration with Supertest assertion library for /GET/ :id to get a request and test fail

--- server/tests/app.spec.js
+++ server/tests/app.spec.js
@@ -1,12 +1,13 @@
 import { expect } from 'chai';
-// import router from '../routes/route';
+import request from 'supertest';
+import express from 'express';
 import app from '../app';
 import Request from '../controllers/requestController';
+// import router from '../routes/route';
 // import requests from '../models/db';
 // import chaiHttp from 'chai-http';
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-
 
 chai.use(chaiHttp);
 
@@ -24,32 +25,42 @@ describe('Requests Controller Test Suite', () => {
         expect(res).to.have.status(404);
       }));
   });
-  describe('/GET/requests/:id ', () => {
-    it('it should GET a request by the given id', (done) => {
-      const request = new Request({
-        id: 1,
-        date: '15-05-2018',
-        department: 'Peoples and Culture',
-        location: 'Receptionist desk',
-        contact: '0908765344',
-        status: 'Approved',
-      });
-      request.save(() => {
-        chai.request(app)
-          .get(`/requests/${request.id}`)
-          .send(request)
-          .end((res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('object');
-            res.body.should.have.property('id').eql(request.id);
-            res.body.should.have.property('date');
-            res.body.should.have.property('department');
-            res.body.should.have.property('location');
-            res.body.should.have.property('contact');
-            res.body.should.have.property('status');
-            done();
-          });
-      });
+
+  // describe('/GET/requests/:id ', () => {
+  //   it('it should GET a request by the given id', () => chai.request(app)
+  //     .get(`api/requests/${request.id}`)
+  //     .then((res) => {
+  //       expect(res).have.status(200);
+  //       expect(res).to.have.property('id').equal('2');
+  //       expect(res).to.have.property('date').equal('15-05-2018');
+  //       expect(res).to.have.property('department').equal('Peoples and Culture');
+  //       expect(res).to.have.property('location').equal('Receptionist desk');
+  //       expect(res).to.have.property('contact').equal('0908765344');
+  //       expect(res).to.have.property('status').equal('Approved');
+  //     }));
+  // });
+});
+
+
+/* ===== Integration testing====  */
+describe('Integrationtesting with supertest for requestController', () => {
+  describe('GET /requests', () => {
+    it('respond with json', (done) => {
+      request(app)
+        .get('/api/requests')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+    });
+  });
+  describe('GET /user/:id', () => {
+    it('respond with json containing a single request', (done) => {
+      request(app)
+        .get('/api/requests/R001')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, done);
     });
   });
 });
+
