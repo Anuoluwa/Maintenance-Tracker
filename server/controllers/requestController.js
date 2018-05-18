b8ab8b28d53fb4bba5ece010cf4671abe07c6533
Anu Apiti <anuapt@gnail.com>
2018-05-18 04:08:27 -0700

    [Feature #157560551] Add test for POST endpoints and a function to pass the test

--- server/controllers/requestController.js
+++ server/controllers/requestController.js
@@ -1,3 +1,5 @@
+import express from 'express';
+import app from '../app';
 /* eslint eqeqeq: ["error", "smart"] */
 import requests from '../models/db';
 
@@ -17,5 +19,18 @@ export default class Request {
       res.status(404).json({ message: 'No request found' });
     }
     res.status(200).json(requestItem);
+  }
+
+  static createRequest(req, res) {
+    const request = {
+      id: requests.length + 1,
+      date: req.body.date,
+      department: req.body.department,
+      location: req.body.location,
+      contact: req.body.contact,
+      status: req.body.status,
+    };
+    requests.push(request);
+    return res.status(200).json(requests);
   }
 }
