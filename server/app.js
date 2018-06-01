3760ae828f78f336b588a8aafdd64818817c8d43
Anu Apiti <anuapt@gnail.com>
2018-06-01 10:57:25 +0200

    fix bug

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
 import user from './routes/user';
@@ -5,10 +6,11 @@ import index from './db';
 import auth from './routes/auth';
 import request from './routes/request';
 
-require('dotenv').config();
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
