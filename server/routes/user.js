94b6f5d82d0efc83463aea0ac70905e3def93a0e
Anu Apiti <anuapt@gnail.com>
2018-06-06 18:54:47 +0200

    [Feature 157998049] Protect routes with jwt token

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,9 +5,9 @@ import { validateBody, schemas } from '../helpers/routeHelpersAuth';
 
 const router = express.Router();
 
-router.post('/requests', User.createRequests);
-router.get('/requests', User.getRequests);
-router.get('/requests/:id', User.getRequest);
-router.put('/requests/:id', User.editRequest);
+router.post('/requests', verifyToken, User.createRequests);
+router.get('/requests', verifyToken, User.getRequests);
+router.get('/requests/:id', verifyToken, User.getRequest);
+router.put('/requests/:id', verifyToken, User.editRequest);
 
 export default router;
